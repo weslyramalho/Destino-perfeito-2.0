36fd71bde3dcb502613c0d4c85683b4b484fcc75
weslyramalho <weslyramalho@yahoo.com.br>
2025-03-25 17:49:19 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Header from './components/Header';
 import Pesquisa from './components/Pesquisa';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className='container'>
      <Header/>
      <Pesquisa />
+     <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import "./styled.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer_ text-light ">
+      <div className="container">
+        <div className="row d-flex align-itens-center justify-content-center m-4">
+          <div className="col-md-6 d-flex align-itens-center justify-content-center m-4">
+            <p>© 2025 Destinoperfeito</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
